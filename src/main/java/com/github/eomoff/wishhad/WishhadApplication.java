5ff07c7780aa5db0b1520d16f025658e5581d354
eomoff <eomoff@gmail.com>
2026-06-19 19:01:40 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/github/eomoff/wishhad/WishhadApplication.java
@@ -0,0 +1,13 @@
+package com.github.eomoff.wishhad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishhadApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WishhadApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/eomoff/wishhad/WishhadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eomoff.wishhad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishhadApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
